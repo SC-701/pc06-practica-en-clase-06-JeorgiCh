bdb8b5f9d3a788a585a5e60b35e0d1d090f425f1
JeorgiCh <jorgech3005@gmail.com>
2026-03-02 00:39:21 -0600

    Se copia la practica 5 para inciar la practica 6

---
+++ Vehiculos/Abstracciones/Interfaces/Flujo/IVehiculoFlujo.cs
@@ -0,0 +1,19 @@
+﻿using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Interfaces.Flujo
+{
+    public interface IVehiculoFlujo
+    {
+        Task<IEnumerable<VehiculoResponse>> Obtener();
+        Task<VehiculoDetalle> ObtenerPorId(Guid Id);
+        Task<Guid> Agregar(VehiculoRequest vehiculo);
+        Task<Guid> Editar(Guid Id, VehiculoRequest vehiculo);
+        Task<Guid> Eliminar(Guid Id);
+    }
+}
---
+++ Vehiculos/BD/dbo/Stored Procedures/EliminarVehiculo.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE EliminarVehiculo
+	-- Add the parameters for the stored procedure here
+	@Id uniqueIdentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	BEGIN TRANSACTION
+	DELETE 
+	FROM  Vehiculos   
+	WHERE  (Vehiculos.Id = @Id)
+	SELECT @Id
+	COMMIT TRANSACTION
+END
